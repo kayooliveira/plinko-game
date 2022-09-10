c7d73156274a7946ce752cc8d17c0eac6b6c6bde
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-10 02:30:26 -0300

    feat(Updating to react 18 and adding Redux Provider around Routes): :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import './styles/global.css'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from 'redux/store'
+import { Routes } from 'routes'
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById('root')!
 
-import { Plinko } from './components/Plinko'
+const root = createRoot(container)
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-    <Plinko />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </React.StrictMode>
 )
