71deb9e45f75e4156b89f01122066c962cd4ea0f
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-10 02:26:28 -0300

    feat(Adding colors to styles): :sparkles:

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  background: '#0f212e',
+  primary: '#213743',
+  secondary: '#3d5564',
+  text: '#F2F7FF',
+  green: '#1fff20',
+  greenDark: '#1ccc20',
+  redDarker: '#ff003f',
+  redDark: '#ff1837',
+  red: '#ff302f',
+  orangeDark: '#ff4827',
+  orange: '#ff6020',
+  orangeLight: '#ff7818',
+  yellowDark: '#ff9010',
+  yellow: '#ffa818',
+  yellowLight: '#ffc000'
+} as const
