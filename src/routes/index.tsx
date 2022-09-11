5e456ad511ffcd499d455dc9bbcf75a7db2ccf38
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-11 17:18:52 -0300

    feat(Adding defaultLayout around routes): :sparkles:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
+import { DefaultLayout } from 'layouts/DefaultLayout'
 import { GamePlinkoPage } from 'pages/Games/Plinko'
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom'
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" element={<GamePlinkoPage />} />
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<GamePlinkoPage />} />
+        </Route>
       </Switch>
     </BrowserRouter>
   )
