6237a9260ac1c86b73cee7e848bc353d57bc77c4
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 18:27:15 -0300

    feat(Adding Event listener to update user wallet balance on change in realtiume): :sparkles:

--- src/pages/Games/Plinko/index.tsx
+++ src/pages/Games/Plinko/index.tsx
@@ -1,4 +1,22 @@
 import { Plinko } from 'components/Plinko'
+import { onValue, ref } from 'firebase/database'
+import { database } from 'lib/firebase'
+import { useDispatch, useSelector } from 'react-redux'
+import { useAuth } from 'redux/slicers/sliceAuth'
+import { setCurrentBalance } from 'redux/slicers/sliceWallet'
 export function GamePlinkoPage() {
+  const { user, isAuth } = useSelector(useAuth)
+  const dispatch = useDispatch()
+
+  const walletRef = ref(database, 'wallet/' + user.id)
+
+  onValue(walletRef, snapshot => {
+    if (snapshot.exists()) {
+      const data = snapshot.val()
+      if (data.currentBalance && isAuth) {
+        dispatch(setCurrentBalance(data.currentBalance))
+      }
+    }
+  })
   return <Plinko />
 }
