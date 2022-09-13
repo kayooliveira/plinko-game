e0e6a06270900a3323ba5b290a822371023fb46d
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 19:22:12 -0300

    feat(dispatching getCurrentBalanceFromDb on IsAuthState changed): :sparkles:

--- src/pages/Games/Plinko/index.tsx
+++ src/pages/Games/Plinko/index.tsx
@@ -1,15 +1,22 @@
 import { Plinko } from 'components/Plinko'
 import { onValue, ref } from 'firebase/database'
 import { database } from 'lib/firebase'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useAuth } from 'redux/slicers/sliceAuth'
-import { setCurrentBalance } from 'redux/slicers/sliceWallet'
+import {
+  getCurrentBalanceFromDb,
+  setCurrentBalance
+} from 'redux/slicers/sliceWallet'
 export function GamePlinkoPage() {
   const { user, isAuth } = useSelector(useAuth)
   const dispatch = useDispatch()
-
+  useEffect(() => {
+    if (isAuth) {
+      dispatch(getCurrentBalanceFromDb())
+    }
+  }, [isAuth])
   const walletRef = ref(database, 'wallet/' + user.id)
-
   onValue(walletRef, snapshot => {
     if (snapshot.exists()) {
       const data = snapshot.val()
