db2ddcf92660de8853a918c3b24dba3eb7cb64c2
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 18:28:05 -0300

    feat(Refactoring WalletCard component): :sparkles:

--- src/layouts/DefaultLayout/components/WalletCard/index.tsx
+++ src/layouts/DefaultLayout/components/WalletCard/index.tsx
@@ -1,31 +1,19 @@
 import { CurrencyDollarSimple } from 'phosphor-react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useAuth } from 'redux/slicers/sliceAuth'
-import { getCurrentBalanceFromDb, useWallet } from 'redux/slicers/sliceWallet'
+import { formatPoints } from 'utils/currencyFormat'
 
 interface WalletCardProps {
   showFormatted?: boolean
+  balance: number
 }
-export function WalletCard({ showFormatted }: WalletCardProps) {
-  const { currentBalance, currentBalanceFormatted } = useSelector(useWallet)
-  const {
-    user: { id }
-  } = useSelector(useAuth)
-  const currency = showFormatted ? currentBalanceFormatted : currentBalance
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch<any>(getCurrentBalanceFromDb(id))
-  }, [id])
-
+export function WalletCard({ balance, showFormatted }: WalletCardProps) {
+  const currency = showFormatted ? formatPoints(balance) : balance
   return (
     <div className="flex cursor-pointer items-stretch">
       <div className="flex items-center gap-2 rounded-tl-md rounded-bl-md bg-background px-2 py-1 pr-4 font-bold uppercase text-white md:text-lg">
         <span className="rounded-full bg-purpleDark p-1">
           <CurrencyDollarSimple weight="bold" />
         </span>
-        <span title={currentBalance.toString()}>{currency}</span>
+        <span title={String(balance)}>{currency}</span>
       </div>
       <span
         title="Plinko Points"
