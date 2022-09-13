6ebff6515859bd87b9f5705840cfc43e1c477be6
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 02:32:58 -0300

    feat(Adding new routes to scoreboard and login): :sparkles:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,22 @@
 import { DefaultLayout } from 'layouts/DefaultLayout'
 import { GamePlinkoPage } from 'pages/Games/Plinko'
+import { LoginPage } from 'pages/Login'
+import { ScoreBoardPage } from 'pages/ScoreBoard'
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom'
+
+import { NotFound } from './components/NotFound'
+import { RequireAuth } from './components/RequireAuth'
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route element={<DefaultLayout />}>
-          <Route path="/" element={<GamePlinkoPage />} />
+          <Route element={<RequireAuth />}>
+            <Route path="/plinko" element={<GamePlinkoPage />} />
+          </Route>
+          <Route path="/" element={<ScoreBoardPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Switch>
     </BrowserRouter>
