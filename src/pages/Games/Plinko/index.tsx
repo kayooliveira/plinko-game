c1d0fb1d155d981f5703d0b2d322c569a67f27c2
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 20:20:55 -0300

    feat(hotfix): :sparkles:

--- src/pages/Games/Plinko/index.tsx
+++ src/pages/Games/Plinko/index.tsx
@@ -1,29 +1,18 @@
 import { Plinko } from 'components/Plinko'
-import { onValue, ref } from 'firebase/database'
-import { database } from 'lib/firebase'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useAuth } from 'redux/slicers/sliceAuth'
-import {
-  getCurrentBalanceFromDb,
-  setCurrentBalance
-} from 'redux/slicers/sliceWallet'
+import { getCurrentBalanceFromDb } from 'redux/slicers/sliceWallet'
 export function GamePlinkoPage() {
   const { user, isAuth } = useSelector(useAuth)
+
   const dispatch = useDispatch()
+
   useEffect(() => {
     if (isAuth) {
       dispatch(getCurrentBalanceFromDb(user.id))
     }
   }, [isAuth, user.id])
-  const walletRef = ref(database, 'wallet/' + user.id)
-  onValue(walletRef, snapshot => {
-    if (snapshot.exists()) {
-      const data = snapshot.val()
-      if (data.currentBalance && isAuth) {
-        dispatch(setCurrentBalance(data.currentBalance))
-      }
-    }
-  })
+
   return <Plinko />
 }
