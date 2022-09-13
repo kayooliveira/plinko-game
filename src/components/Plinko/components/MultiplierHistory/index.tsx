9f098bae904f26d7f7354306e5ed3687da6dbebc
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 20:56:56 -0300

    feat(Updating multiplier counter top margin in small screens): :art:

--- src/components/Plinko/components/MultiplierHistory/index.tsx
+++ src/components/Plinko/components/MultiplierHistory/index.tsx
@@ -5,7 +5,7 @@ export function MultiplierHistory({
   multiplierHistory
 }: MultiplierHistoryProps) {
   return (
-    <div className="absolute right-4 top-60 flex w-16 flex-col gap-1 overflow-hidden rounded-md bg-background">
+    <div className="absolute right-4 top-40 flex w-16 flex-col gap-1 overflow-hidden rounded-md bg-background md:top-60">
       {multiplierHistory.map((multiplier, index) => {
         if (index > 3 || !multiplier) return null
         return (
