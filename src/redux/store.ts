c5bca7527c8fb6e7727fa27b3ec2191710fa25c7
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 02:31:01 -0300

    feat(Adding auth slice to redux store): :sparkles:

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import sliceAuth from './slicers/sliceAuth'
 import sliceWallet from './slicers/sliceWallet'
 
 const store = configureStore({
   reducer: {
-    wallet: sliceWallet
-  }
+    wallet: sliceWallet,
+    auth: sliceAuth
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
 })
 
 export default store
