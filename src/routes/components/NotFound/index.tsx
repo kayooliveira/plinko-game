9a81e17268c0b7ee99109ad2c1f0acce00a39928
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-13 02:33:30 -0300

    feat(Creating notFound page): :sparkles:

---
+++ src/routes/components/NotFound/index.tsx
@@ -0,0 +1,11 @@
+export function NotFound() {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-text">404</h1>
+      <h2 className="text-2xl font-bold text-text">Página não encontrada</h2>
+      <button className="mt-4 rounded-md bg-purpleDark px-4 py-2 font-bold text-text transition-colors hover:bg-purple">
+        Voltar para a página inicial
+      </button>
+    </div>
+  )
+}
