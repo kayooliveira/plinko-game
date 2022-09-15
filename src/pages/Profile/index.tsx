9c1a59237f6348db399df6d7276b0e3535f9e3c3
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-15 16:59:42 -0300

    feat(Adding crown icon to top 3 players): :sparkles:

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,21 +1,51 @@
+import classNames from 'classnames'
+import { Crown } from 'phosphor-react'
+import { useAuthStore } from 'store/auth'
+import { formatPoints } from 'utils/currencyFormat'
+
 interface User {
   name: string
   profilePic: string
   uid: string
   currentBalance: number
+  position: number
 }
 
 export function Profile(user: User) {
+  const authUser = useAuthStore(state => state.user)
+  console.log(user.position)
   return (
-    <div className="flex flex-col items-center justify-center rounded-md bg-primary p-2 px-6 text-text">
-      <img
-        src={user.profilePic}
-        alt={user.name + ' Avatar'}
-        className="mx-auto w-32 rounded-full"
-      />
-      <span className="text-center text-2xl font-bold">{user.name}</span>
+    <div className="flex flex-col items-center justify-center gap-2 rounded-md bg-primary p-2 px-6 text-text">
+      <div className="relative mx-auto w-32 rounded-full">
+        <img
+          src={user.profilePic}
+          alt={user.name + ' Avatar'}
+          className="w-full rounded-full"
+        />
+        {user.position <= 2 && (
+          <Crown
+            className={classNames(
+              'absolute -right-2 bottom-0 transition-colors',
+              {
+                'text-yellow-400': user.position === 0,
+                'text-gray-300': user.position === 1,
+                'text-yellow-800': user.position === 2
+              }
+            )}
+            weight="fill"
+            size="40"
+          />
+        )}
+      </div>
+      <span
+        className={classNames('text-center text-2xl font-bold', {
+          'text-purple': user.uid === authUser.id
+        })}
+      >
+        {user.name} {user.uid === authUser.id && '(você)'}
+      </span>
       <span className="text-center text-xl font-bold">
-        {user.currentBalance} PPs
+        {formatPoints(user.currentBalance)} PPs
       </span>
     </div>
   )
