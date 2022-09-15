8d3ebc86f3bbd6a2413b08e2c6d616cead8ce4dd
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-15 02:16:15 -0300

    revert(Removing Redux Provider): :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import './styles/global.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from 'redux/store'
 import { Routes } from 'routes'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!
@@ -11,8 +9,6 @@ const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
+    <Routes />
   </React.StrictMode>
 )
