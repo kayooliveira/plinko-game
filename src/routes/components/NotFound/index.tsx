eda37ab9ba8a5a9e24e55388b6d2753964aeff67
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-16 17:04:32 -0300

    feat(Updating back button on NotFound page): :sparkles:

--- src/routes/components/NotFound/index.tsx
+++ src/routes/components/NotFound/index.tsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom'
+
 export function NotFound() {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <h1 className="text-4xl font-bold text-text">404</h1>
       <h2 className="text-2xl font-bold text-text">Página não encontrada</h2>
-      <button className="mt-4 rounded-md bg-purpleDark px-4 py-2 font-bold text-text transition-colors hover:bg-purple">
+      <Link
+        to="/"
+        className="mt-4 rounded-md bg-purpleDark px-4 py-2 font-bold text-text transition-colors hover:bg-purple"
+      >
         Voltar para a página inicial
-      </button>
+      </Link>
     </div>
   )
 }
