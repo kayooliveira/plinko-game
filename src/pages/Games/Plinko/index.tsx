e67deefada6a768b971fa1c33e1567ace538afe6
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-16 17:03:26 -0300

    feat(Prevent user reload or close window if has games running): :sparkles:

--- src/pages/Games/Plinko/index.tsx
+++ src/pages/Games/Plinko/index.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react'
+import { useGameStore } from 'store/game'
+
 import { Game } from './components/Game'
 
 export function PlinkoGamePage() {
+  const alertUser = (e: BeforeUnloadEvent) => {
+    console.log('gamesRunning', gamesRunning)
+    if (gamesRunning > 0) {
+      e.preventDefault()
+      alert('Tu quer mermo sair feladaputa?')
+      e.returnValue = ''
+    }
+  }
+  const gamesRunning = useGameStore(state => state.gamesRunning)
+  useEffect(() => {
+    window.addEventListener('beforeunload', alertUser)
+    return () => {
+      window.removeEventListener('beforeunload', alertUser)
+    }
+  }, [gamesRunning])
   return <Game />
 }
