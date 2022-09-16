39e005f0ea7ee9c6282c4b6360e5e91c41132d18
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-09-16 17:01:11 -0300

    feat(Adding toaster from react-hot-toast to main.tsx): :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import './styles/global.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { Routes } from 'routes'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!
 
 const root = createRoot(container)
 
-root.render(<Routes />)
+root.render(
+  <>
+    <Routes />
+    <Toaster />
+  </>
+)
