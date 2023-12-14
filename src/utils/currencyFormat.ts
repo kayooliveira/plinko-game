67db6c7db9a8b05b1e076ac74d5f7da4ed79132b
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-12-14 00:38:21 -0300

    feat(Adding more abbrevs to formatPoints function): :zap:

--- src/utils/currencyFormat.ts
+++ src/utils/currencyFormat.ts
@@ -37,33 +37,75 @@ export function formatPoints(number: number): string {
     ' SepV',
     ' OctV',
     ' NonV',
-    ' TrigV'
+    ' TrigV',
+    ' QuadragV',
+    ' QuinquagV',
+    ' SexagV',
+    ' SeptuagV',
+    ' OctogV',
+    ' NonagV',
+    ' CentV',
+    ' DucentV',
+    ' TrecentV',
+    ' QuadringV',
+    ' QuingentV',
+    ' SexcentV',
+    ' SeptingentV',
+    ' OctingentV',
+    ' NongentV',
+    ' MillV',
+    ' UnDuoMillV',
+    ' DuoDuoMillV',
+    ' TreDuoMillV',
+    ' QuattuorDuoMillV',
+    ' QuinDuoMillV',
+    ' SexDuoMillV',
+    ' SepDuoMillV',
+    ' OctDuoMillV',
+    ' NovemDuoMillV',
+    ' VigintiMillV',
+    'TrigintiMillV',
+  'QuadragintiMillV',
+  'QuinquagintiMillV',
+  'SexagintiMillV',
+  'SeptuagintiMillV',
+  'OctogintiMillV',
+  'NonagintiMillV',
+  'CentumMillV',
+  'DucentumMillV',
+  'TrecentumMillV',
+  'QuadringentumMillV',
+  'QuingentumMillV',
+  'SexcentumMillV',
+  'SeptingentumMillV',
+  'OctingentumMillV',
+  'NongentumMillV',
+  'Millies',
+  'UnDuoMillies',
+  'DuoDuoMillies',
+  'TreDuoMillies',
+  'QuattuorDuoMillies',
+  'QuinDuoMillies',
+  'SexDuoMillies',
+  'SepDuoMillies',
+  'OctDuoMillies',
+  'NovemDuoMillies',
+  'VigintiMillies',
   ]
 
-  // Go through the array backwards, so we do the largest first
   for (let i = abbrev.length - 1; i >= 0; i--) {
-    // Convert array index to "1000", "1000000", etc
     const size = Math.pow(10, (i + 1) * 3)
-
-    // If the number is bigger or equal do the abbreviation
     if (size <= newNumber) {
-      // Here, we multiply by decPlaces, round, and then divide by decPlaces.
-      // This gives us nice rounding to a particular decimal place.
       newNumber = Math.floor((newNumber * decPlaces) / size) / decPlaces
-
-      // Handle special case where we round up to the next abbreviation
       if (newNumber === 1000 && i < abbrev.length - 1) {
         newNumber = 1
         i++
       }
-
-      // Add the letter for the abbreviation
       newNumber += abbrev[i]
-
-      // We are done... stop
       break
     }
   }
 
   return newNumber
 }
+
